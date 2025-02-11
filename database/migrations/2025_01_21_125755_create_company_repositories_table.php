218a43bd8f6d2894919eb783179a93035779dbb4
Kovács Zoltán <kovacs.zoltan@softc.hu>
2025-02-11 13:32:03 +0100

    main - mentés

--- database/migrations/2025_01_21_125755_create_company_repositories_table.php
+++ database/migrations/2025_01_21_125755_create_company_repositories_table.php
@@ -12,11 +12,16 @@ return new class extends Migration
 	 */
 	public function up()
 	{
-		Schema::create('company_repositories', function(Blueprint $table) {
-            $table->increments('id');
+            Schema::create('company_repositories', function(Blueprint $table) {
+                $table->increments('id');
 
-            $table->timestamps();
-		});
+                $table->string('name', 255);
+                $table->string('email', 255);
+                $table->string('address', 255);
+                $table->string('phone', 255);
+                
+                $table->timestamps();
+            });
 	}
 
 	/**
@@ -26,6 +31,6 @@ return new class extends Migration
 	 */
 	public function down()
 	{
-		Schema::drop('company_repositories');
+            Schema::drop('company_repositories');
 	}
 };
